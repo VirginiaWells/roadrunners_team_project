445b69dcbea0c4551ea0c32e386e292830d522ef
Virginia Hudson <VirginiaWellsHudson@gmail.com>
2018-11-01 15:03:21 -0500

    comment

--- Group Project/Car.java
+++ Group Project/Car.java
@@ -60,9 +60,21 @@ public class Car extends Actor
         if (gas != null) {
             getWorld().removeObject(gas);
             gasMeter++;
-            if (gasMeter >= 20) {
-                Greenfoot.stop();
+            if (gasMeter == 1) { //changed to 1 for testing
+                getWorld().addObject (new finishLine(), 508, 0);
             }
+        }
+        Actor finish = getOneIntersectingObject(finishLine.class);
+        if (finish != null) {
+            getWorld().addObject (new fireworks(), 400, 700);
+            getWorld().addObject (new fireworks(), 800, 800);
+            getWorld().addObject (new fireworks(), 900, 850);
+            getWorld().addObject (new fireworks(), 300, 775);
+            getWorld().addObject (new fireworks(), 200, 800);
+            getWorld().addObject (new fireworks(), 100, 700);
+            getWorld().addObject (new fireworks(), 200, 750);
+            Greenfoot.playSound("finish.mp3");
+            Greenfoot.stop();
         }
     }    
     public void splishSplash() {
---
+++ Group Project/fireworks.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class fireworks here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class fireworks extends Actor
+{
+    /**
+     * Act - do whatever the fireworks wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
